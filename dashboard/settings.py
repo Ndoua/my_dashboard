1b6d2f2a7eaeb0b6468b1a6574abcea71c4d18c8
Ndoua Serge <serge.ndoua@outlook.com>
2018-11-29 15:49:28 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'my_profile/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
